2ba9172c5c2519ebd6599bce2c9beec283152f61
MNiko97 <nikola.mitrovic@ulb.ac.be>
2020-04-16 22:26:47 +0200

    update

--- 2BA/Informatic/Numerical Computing/Coding/Coding_1.py
+++ 2BA/Informatic/Numerical Computing/Coding/Coding_1.py
@@ -1,20 +1,24 @@
+
 # transformpict.py
 # Author: Sébastien Combéfis
 # Version: April 15, 2020
 
-import imageio
+import imageio, os
 import scipy as np
 
 # Opening the tiger picture you can find here:
 # https://www.flickr.com/photos/31004716@N00/3732644607
 # The returned ndarray has three dimensions: the width, the height and
 # the number of layers (3 for RGB picture, 4 for RGBA picture, etc.)
-im = imageio.imread('tiger.jpg')
+ROOT = os.path.abspath(os.getcwd()) + "/2BA/Informatic/Numerical Computing/Coding/"
+im = imageio.imread(ROOT + 'tiger.jpg')
+print(im)
 
 # Darkening the picture by dividing the RGB values of each pixel by 2
 # and ensuring the value remains in [0; 255]
-im = np.maximum(0, im / 2)
+im = np.maximum(0, im/2)
+print(im)
 
 # Writing the new picture without forgetting to convert the values
 # from float to uint8 (because of the operations previously performed)
-imageio.imwrite('tiger-transformed.jpg', im.astype(np.uint8))
+imageio.imwrite(ROOT + 'tiger-transformed.jpg', im.astype(np.uint8))
