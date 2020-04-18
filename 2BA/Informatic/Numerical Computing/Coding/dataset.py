7db6511f54ab71ce45e4ea7ad46173220e57aa77
MNiko97 <nikola.mitrovic@ulb.ac.be>
2020-04-19 00:44:30 +0200

    update

--- 2BA/Informatic/Numerical Computing/Coding/dataset.py
+++ 2BA/Informatic/Numerical Computing/Coding/dataset.py
@@ -2,7 +2,52 @@
 # Author: Mitrovic Nikola
 # Version: April 16, 2020
 
-import openpyxl
+import openpyxl, os, datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.ticker.formatter as formatter
+import matplotlib.ticker.locator as locator
+import numpy as np
+
+ROOT = os.path.abspath(os.getcwd()) + "/2BA/Informatic/Numerical Computing/Coding/"
 
 # Opening COVID19 dataset in xlsx format:
 # Plot data analyses
+
+book = openpyxl.load_workbook(ROOT + 'COVID19BE.xlsx')
+sheet = book.get_sheet_by_name('MORT')
+shape = (sheet.max_row, 4)
+
+data = []
+death = 0
+i = 2
+
+while i < sheet.max_row :
+    death += sheet.cell(row = i, column = 5).value
+    date = sheet.cell(row = i, column = 1).value
+    if date == sheet.cell(row = i+1, column = 1).value :
+        death += sheet.cell(row = i+1, column = 5).value
+    if date != sheet.cell(row = i+1, column = 1).value :
+        data.append([date, death])
+        death = 0
+    i+=1
+
+# fig, ax = plt.subplots(figsize=(12, 12))
+dates = []
+for i in range(len(data)):
+    dates.append(data[i][0])
+x_values = [datetime.datetime.strptime(d, "%Y-%m-%d").date() for d in dates]
+y_values = [item[1] for item in data]
+ax = plt.gca()
+formatter = mdates.DateFormatter("%m-%d")
+ax.xaxis.set_major_formatter(formatter)
+locator = mdates.DayLocator()
+ax.xaxis.set_major_formatter(locator)
+plt.plot(x_values, y_values[:])
+
+# date_form = ("%m-%d")
+# fig, ax = plt.subplot(figsize=())
+# ax.xaxis.set_major_formatter(date_form)
+# plt.plot(data[:,2], data[:, 3])
+# plt.grid()
+# plt.show()
--- 2BA/Informatic/Numerical Computing/Coding/transformpict.py
+++ 2BA/Informatic/Numerical Computing/Coding/transformpict.py
@@ -3,21 +3,39 @@
 # Version: April 15, 2020
 
 import imageio, os
-import scipy as np
+import numpy as np
+import copy
 
 # Opening the tiger picture you can find here:
 # https://www.flickr.com/photos/31004716@N00/3732644607
 # The returned ndarray has three dimensions: the width, the height and
 # the number of layers (3 for RGB picture, 4 for RGBA picture, etc.)
 ROOT = os.path.abspath(os.getcwd()) + "/2BA/Informatic/Numerical Computing/Coding/"
-im = imageio.imread(ROOT + 'tiger.jpg')
-print(im)
+image = 'tiger.jpg'
 
-# Darkening the picture by dividing the RGB values of each pixel by 2
+# Darkening the picture by dividing the RGB values of each pixel by n
 # and ensuring the value remains in [0; 255]
-im = np.maximum(0, im/2)
-print(im)
-
 # Writing the new picture without forgetting to convert the values
 # from float to uint8 (because of the operations previously performed)
-imageio.imwrite(ROOT + 'tiger-transformed.jpg', im.astype(np.uint8))
+def dark(image, n):
+    im = imageio.imread(ROOT + image)
+    im = np.maximum(0, image/n)
+    imageio.imwrite(ROOT + 'tiger-transformed.jpg', im.astype(np.uint8))
+    return im
+
+def invertcolor(image):
+    im = imageio.imread(ROOT + image)
+    inverted = [255, 255, 255] - im[:][:]
+    imageio.imwrite(ROOT + 'tiger-transformed.jpg', inverted.astype(np.uint8))
+    return inverted
+
+def crop(image, point1, point2):
+    im = imageio.imread(ROOT + image)
+    x1, y1 = point1
+    x2, y2 = point2
+    cropped = im[x1:x2, y1:y2]
+    imageio.imwrite(ROOT + 'tiger-transformed.jpg', cropped.astype(np.uint8))
+    return cropped
+    
+#print(crop(image, (0, 0), (500, 500)).shape)
+#print(invertcolor(image).shape)
