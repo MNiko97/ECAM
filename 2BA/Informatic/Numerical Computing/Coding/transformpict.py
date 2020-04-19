5d49117c69683be602de8fea27a15d24cadd3964
MNiko97 <nikola.mitrovic@ulb.ac.be>
2020-04-19 15:30:18 +0200

    final version

--- 2BA/Informatic/Numerical Computing/Coding/transformpict.py
+++ 2BA/Informatic/Numerical Computing/Coding/transformpict.py
@@ -1,10 +1,9 @@
 # transformpict.py
-# Author: Sébastien Combéfis
-# Version: April 15, 2020
+# Author: Mitrovic Nikola
+# Version: April 19, 2020
 
 import imageio, os
 import numpy as np
-import copy
 
 # Opening the tiger picture you can find here:
 # https://www.flickr.com/photos/31004716@N00/3732644607
@@ -12,30 +11,31 @@ import copy
 # the number of layers (3 for RGB picture, 4 for RGBA picture, etc.)
 ROOT = os.path.abspath(os.getcwd()) + "/2BA/Informatic/Numerical Computing/Coding/"
 image = 'tiger.jpg'
+im = imageio.imread(ROOT + image)
 
-# Darkening the picture by dividing the RGB values of each pixel by n
+# Darkening the picture by dividing the RGB values of each pixel by n %
 # and ensuring the value remains in [0; 255]
-# Writing the new picture without forgetting to convert the values
-# from float to uint8 (because of the operations previously performed)
 def dark(image, n):
-    im = imageio.imread(ROOT + image)
-    im = np.maximum(0, image/n)
-    imageio.imwrite(ROOT + 'tiger-transformed.jpg', im.astype(np.uint8))
-    return im
-
+    factor = 100/n
+    return np.maximum(0, image/factor)
+    
+# To invert color in a picture :
+# Substract maximum value (255) to current value
+# For each R, G, B level value of each pixels composing the image
 def invertcolor(image):
-    im = imageio.imread(ROOT + image)
-    inverted = [255, 255, 255] - im[:][:]
-    imageio.imwrite(ROOT + 'tiger-transformed.jpg', inverted.astype(np.uint8))
-    return inverted
+    return [255, 255, 255] - image[:][:]
 
+# Crop image given two points coordinate that form borders
+# By only saving pixels between these borders
 def crop(image, point1, point2):
-    im = imageio.imread(ROOT + image)
     x1, y1 = point1
     x2, y2 = point2
-    cropped = im[x1:x2, y1:y2]
-    imageio.imwrite(ROOT + 'tiger-transformed.jpg', cropped.astype(np.uint8))
-    return cropped
-    
-#print(crop(image, (0, 0), (500, 500)).shape)
-#print(invertcolor(image).shape)
+    return image[y1:y2, x1:x2]
+
+# Testing all image processing methods : 
+# Inverting color, cropping image and
+# Darkening to 50% of the original value
+inverted_image = invertcolor(im) 
+cropped_image = crop(inverted_image, (325, 65), (1120, 905))
+final_image = dark(cropped_image, 50)
+imageio.imwrite(ROOT + 'tiger-transformed.jpg', final_image.astype(np.uint8))
