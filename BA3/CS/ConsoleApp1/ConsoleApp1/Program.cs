df0fe6e393cea6b6ab21dfa7d379f1248c80b7fe
MNiko97 <nikola.mitrovic@ulb.ac.be>
2020-09-25 10:14:17 +0200

    update

---
+++ BA3/CS/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name : ");
+            string a = Console.ReadLine();
+            Console.WriteLine(a);
+        }
+    }
+}
