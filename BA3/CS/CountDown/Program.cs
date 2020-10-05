b2a6768b4215f36845c6d6bfc10c63e74dbb90b0
MNiko97 <nikola.mitrovic@student.ecam.be>
2020-10-05 09:13:25 +0200

    update

---
+++ BA3/CS/CountDown/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace CountDown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountDown countDown = new CountDown(10);
+            countDown.Start();
+        }
+    }
+
+    class CountDown
+    {
+        private int count;
+        
+        public CountDown(int count){
+            this.count = count;
+        }
+        public void Start(){
+            Console.WriteLine(count);
+            count--;
+            if (count == 0){
+                Stop();
+            }
+        }
+
+        public void Stop(){
+
+        }
+
+        public int RemainingTime{
+            get{
+                return count;
+            }
+        }
+    }
+}
