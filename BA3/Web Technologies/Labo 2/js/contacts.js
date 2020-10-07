de254d81fc4bbaeccf755e1b24ec0a049570a431
MNiko97 <nikola.mitrovic@ulb.ac.be>
2020-10-08 01:00:36 +0200

    update

--- BA3/Web Technologies/Labo 2/js/contacts.js
+++ BA3/Web Technologies/Labo 2/js/contacts.js
@@ -3,7 +3,8 @@ Activity : Managing Contacts
 */
 
 // Global Variables
-let running = true;
+var running = true;
+var newContactOption = false;
 
 class ContactList{
     constructor(){
@@ -33,49 +34,86 @@ class ContactList{
         }
     }
 }
-
 class Contact{
     constructor(firstname, name){
         this.firstname = firstname;
         this.name = name;
     }
     showInfo(){
-        console.log(this.firstname + " " + this.name);
+        console.log("Name: " + this.name + ", first name: " + this.firstname);
     }
 }
-
 function showMenu(){
+    console.log("");
     console.log("1 : Show Contact List");
     console.log("2 : Add New Contact");
     console.log("0 : Quit");
+    console.log("");
 }
-
-function main(){
-    let myContactList = new ContactList;
-    myContactList.addNewContact("Carole", "Lévisse");
-    myContactList.addNewContact("Mélodie", "Nelsonne");
-    console.log("Welcome !")
-    while(running){
-        showMenu();
-        const input = prompt();
-        switch (input){
-            case "1":
-                myContactList.showAllContacts();
-                break;
-            case "2":
-                console.log("New contact: ");
-                console.log("Write name: ");
-                const name = prompt();
-                console.log("Write Firstname : ");
-                const firstname = prompt();
-                myContactList.addNewContact([name, firstname]);
-                console.log("Contact sucessfully added!")
-                myContactList.showAllContacts();
-                break;
-            case "0":
-                running = False;
-                break;
+const init = function(){
+    document.getElementById('button-submit').addEventListener('click', send);
+}
+const send = function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    let currentOption = validateOption();
+    if (currentOption && running && !newContactOption){
+        inputOption = document.getElementById("input-option").value;
+        if (inputOption == "1"){
+            myContactList.showAllContacts();
+            document.getElementById("option-info").innerHTML = "Show Contacts";
+            showMenu();
+        }
+        if (inputOption == "2"){
+            document.getElementById("option-info").innerHTML = "New Contact";
+            document.getElementById("input-name").disabled = false;
+            document.getElementById("input-firstname").disabled = false;
+            document.getElementById("input-option").disabled = true;
+            let firstname = document.getElementById('input-firstname').value;
+            let name = document.getElementById('input-name').value;
+            let newContact = [];
+            if (firstname.match('^[A-Za-z ]+$') && firstname.length != 0){
+                newContact.push(firstname);
+            }
+            if (name.match('^[A-Za-z ]+$') && name.length != 0){
+                newContact.push(name);
+            }
+            if (newContact.length == 2){
+                myContactList.addNewContact(firstname, name);
+                console.log("New contact successfully added !");
+                showMenu();
+                document.getElementById("form-user").reset();
+                document.getElementById("option-info").innerHTML = "Option";
+                document.getElementById("input-name").disabled = true;
+                document.getElementById("input-firstname").disabled = true;
+                document.getElementById("input-option").disabled = false;
+                newContactOption = false;
+            }
+        }
+        if (inputOption == "0"){
+            running = false;
+            document.getElementById("form-user").style.visibility = "hidden";
+            document.getElementById("main-title").innerHTML = "Thank you for using our App!";
+            console.log("You quit the contact manager, bye!");          
         }
     }
+    else if (!currentOption){
+        console.log("Wrong Input!");
+        showMenu();
+    }
 }
-
+const validateOption = function(e){
+    let input = document.getElementById("input-option").value;
+    if (input.match('^[0-2]{1}$')){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+document.addEventListener("DOMContentLoaded", init);
+let myContactList = new ContactList;
+myContactList.addNewContact("Carole", "Lévisse");
+myContactList.addNewContact("Mélodie", "Nelsonne");
+console.log("Welcome to Contact Manager App!");
+showMenu()
