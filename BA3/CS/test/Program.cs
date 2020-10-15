3d6edf96de7e8c9da0b44a950467b603d648f248
MNiko97 <nikola.mitrovic@ulb.ac.be>
2020-10-15 09:08:21 +0200

    update

--- BA3/CS/test/Program.cs
+++ BA3/CS/test/Program.cs
@@ -2,11 +2,22 @@
 
 namespace test
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+    class Person {
+        public override string ToString() {
+        return "je suis une personne";
+        }
+    }
+
+    class Employee : Person {
+        public override string ToString() {
+            return "je suis un employé";
+        }
+    }
+
+    class Program {
+        public static void Main(string [] args) {
+            Person e = new Employee();
+            Console.WriteLine(e);
         }
     }
 }
